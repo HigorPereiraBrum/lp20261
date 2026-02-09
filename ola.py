645fcffb45c82d3360d7141232a2b924a0b4f1e2
HigorPereiraBrum <higorbrum40@gmail.com>
2026-02-09 15:07:33 +0000

    Teste de input e corversão de variaveis.

--- ola.py
+++ ola.py
@@ -1,1 +1,4 @@
-print('ola,mundo!!!')
+nome = input("Digite seu nome: ")
+print(f"Olá {nome}!! Seja Bem-Vindo!! ")
+idade = int(input("Por favor digite sua idade: "))
+print(f"Perfeito {nome}.Você tem {idade} anos!! ")
