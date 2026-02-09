90374b063c3abd42be545d67c333667e06e0d4d2
HigorPereiraBrum <higorbrum40@gmail.com>
2026-02-09 15:16:37 +0000

    atualizado

--- ola.py
+++ ola.py
@@ -1,4 +1,7 @@
-nome = input("Digite seu nome: ")
-print(f"Olá {nome}!! Seja Bem-Vindo!! ")
-idade = int(input("Por favor digite sua idade: "))
-print(f"Perfeito {nome}.Você tem {idade} anos!! ")
+def diga_ola():
+    nome = input("Digite seu nome: ")
+    print(f"Olá {nome}!! Seja Bem-Vindo!! ")
+    idade = int(input("Por favor digite sua idade: "))
+    print(f"Perfeito {nome}.Você tem {idade} anos!! ")
+
+diga_ola()
